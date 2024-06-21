a34e1f0d2de03264b08258e551b19cbe24cb8963
RameezRizvan <rameezrizvan04@gmail.com>
2024-06-21 16:08:36 +0530

    fixtures added

---
+++ frontend/src/Pages/Countries.jsx
@@ -0,0 +1,50 @@
+
+import germany from "../assets/flags/germany.png";
+import {  useNavigate } from "react-router-dom";
+import {useFrappeGetDocList} from 'frappe-react-sdk'
+import { useContext } from "react";
+import { countryContext } from "../Components/ContextShare";
+
+function Countries({ show }) {
+   show(true);
+
+   const {data,error} = useFrappeGetDocList('Countries')
+   console.log(error);
+   const { countryData, setCountryData } = useContext(countryContext)
+   const navigate = useNavigate()
+   const goToCountry = (country) =>{
+      setCountryData(country)
+      console.log(countryData);
+      // navigate("/universities")
+   }
+
+   return (
+      <section id="countriesSection" className="container">
+         <div className="countriesContainer shapeParent">
+            <div className="my-5">
+               <div className="shape"></div>
+               <h3 className="ms-4">Countries</h3>
+            </div>
+            <div className="d-flex flex-wrap justify-content-center align-items-center column-gap-3 my-5">
+                  {data?.map((country,i)=>(
+                     <div onClick={()=>goToCountry(country)} style={{ textDecoration: "none" }} key={i}>
+                           <div>
+                           <img
+                              src={germany}
+                              height={180}
+                              alt="Country pic"
+                              className="rounded border"
+                           />
+                           <div className="title text-center">
+                              <p>{country.name}</p>
+                           </div>
+                        </div>
+                     </div>
+                  ))}
+            </div>
+         </div>
+      </section>
+   );
+}
+
+export default Countries;
---
+++ frontend/src/Pages/Universities.jsx
@@ -0,0 +1,65 @@
+import { useNavigate } from "react-router-dom";
+import university1 from "../assets/universities/university1.svg";
+
+import { useFrappeGetDocList } from "frappe-react-sdk";
+import { useContext, useEffect, useState } from "react";
+import { countryContext, universityContext } from "../Components/ContextShare";
+
+function Universities({ show }) {
+  show(true);
+  const [filters, setFilters] = useState([]);
+  const navigate = useNavigate();
+  const { unviersityData, setUniversityData } = useContext(universityContext);
+  const { countryData, setCountryData } = useContext(countryContext);
+  if (countryData != "") {
+     setFilters(["country", "=", countryData.country]);
+   }
+   console.log(filters);
+  const { data, error } = useFrappeGetDocList("University", {
+    fields: ["country", "university", "type_of_university", "location"],
+  });
+  console.log(data, error);
+
+  const gotoUniversity = (university) => {
+    console.log("Here");
+    setUniversityData(university);
+    navigate("/university");
+  };
+
+  return (
+    <section id="universitiesSection" className="container">
+      <div className="universitiesContainer shapeParent my-5">
+        <div className="my-5">
+          <div className="shape"></div>
+          <h3 className="ms-4">Universities</h3>
+        </div>
+        <div className="universityList d-flex justify-content-center align-items-center gap-5 flex-wrap">
+          {data?.map((university, i) => (
+            <div
+              to="/university"
+              style={{ textDecoration: "none", color: "black" }}
+              key={i}
+              onClick={() => gotoUniversity(university)}
+            >
+              <div
+                className=" universitycard shadow p-3 rounded"
+                style={{ width: "18rem" }}
+              >
+                <img
+                  src={university1}
+                  alt="university1"
+                  style={{ width: "100%" }}
+                  className="rounded mb-3"
+                />
+                <p className="unviversitytitile">In {university.country}</p>
+                <p className="universityfname">{university.university}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Universities;
